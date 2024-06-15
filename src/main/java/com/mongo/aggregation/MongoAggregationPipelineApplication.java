596846bbfc77122656f52e74f8bf7520f7d5da73
Abhishek Jadon <jadonabhi0@gmail.com>
2024-06-15 23:32:46 +0530

    initial commit

---
+++ src/main/java/com/mongo/aggregation/MongoAggregationPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.mongo.aggregation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoAggregationPipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoAggregationPipelineApplication.class, args);
+    }
+
+}
